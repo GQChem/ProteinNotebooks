e7ee1680b2541c82b5e44b9b363cd99e296dae84
GQChem <161027511+GQChem@users.noreply.github.com>
2024-04-08 13:09:32 +0200

    Add files via upload

---
+++ InstallationFiles/protein_notebooks.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+echo
+echo E. HELP SCRIPTS AND NOTEBOOKS
+cd ..
+if [ ! -d "ProteinNotebooks" ]; then
+    git clone https://github.com/GQChem/ProteinNotebooks
+else
+    if [ -d "_BackupPDBs" ]; then
+        rm -r _BackupPDBs
+    fi
+    if [ -d "_BackupPipelines" ]; then
+        rm -r _BackupPipelines
+    fi
+    mv ProteinNotebooks/PDBs _BackupPDBs
+    mv ProteinNotebooks/Pipelines _BackupPipelines
+    rm -rf ProteinNotebooks
+    git clone https://github.com/GQChem/ProteinNotebooks
+    rm -rf ProteinNotebooks/PDBs
+    rm -rf ProteinNotebooks/Pipelines
+    mv _BackupPDBs ProteinNotebooks/PDBs
+    mv _BackupPipelines ProteinNotebooks/Pipelines
+    rm -rf _BackupPDBs
+    rm -rf _BackupPipeline
+fi
+
+echo
+echo DONE!
